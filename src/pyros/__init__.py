7c9a3d3fd7bfff129ebd9a0c43741a937bc727bd
alexv <asmodehn@gmail.com>
2015-12-15 10:55:53 +0900

    exposing pyros service exceptions for import.

--- src/pyros/__init__.py
+++ src/pyros/__init__.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 
 from .exceptions import PyrosException
 from .pyros_ctx_server import pyros_ctx
-from .pyros_client import PyrosClient
+from .pyros_client import PyrosClient, PyrosServiceNotFound, PyrosServiceTimeout
 
 __all__ = [
     'PyrosException',
+    'PyrosServiceNotFound',
+    'PyrosServiceTimeout',
     'pyros_ctx',
     'PyrosClient',
 ]
