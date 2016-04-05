0b4854eedc869e9db3ae53acb4363246d86b0388
alexv <asmodehn@gmail.com>
2016-04-05 09:52:16 +0900

    now using catkin_pure_python

--- setup.py
+++ setup.py
@@ -1,77 +1,36 @@
-# This setup is usable by catkin, or on its own as usual python setup.py
+from setuptools import setup
 
-_CATKIN = False
-try:
-    from distutils.core import setup
-    from catkin_pkg.python_setup import generate_distutils_setup
-    _CATKIN = True
-except Exception, e:
-    from setuptools import setup
-
-# CAREFUL distutils and setuptools take different arguments and have different behaviors
-# ROS PACKAGING
-if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
-    # fetch values from package.xml
-    setup_args = generate_distutils_setup(
-        packages=[
-            'zmp',
-            'pyros',
-            'pyros.tests',
-            'pyros.baseinterface',
-            'pyros.mockinterface',
-            'pyros.mockinterface.tests',
-            'pyros.rosinterface',
-            'pyros.rosinterface.tests',
-            'pyros.rosinterface.rostests',
-            'pyros.zmpinterface',
-            'pyros.zmpinterface.tests',
-            'tblib',
-        ],
-        package_dir={
-            'zmp': 'src/zmp',
-            'pyros': 'src/pyros',
-            'tblib': 'deps/python-tblib/src/tblib',
-        },
-        py_modules=[
-            'six',
-        ],
-    )
-    setup(**setup_args)
-# PYTHON PACKAGING
-else:  # using setuptools : http://pythonhosted.org/setuptools/
-
-    setup(name='pyros',
-        version='0.1.0',
-        description='ROS Node to provide ROS introspection for non-ROS users.',
-        url='http://github.com/asmodehn/pyros',
-        author='AlexV',
-        author_email='asmodehn@gmail.com',
-        license='BSD',
-        packages=[
-            'zmp',
-            'pyros',
-            'pyros.tests',
-            'pyros.baseinterface',
-            'pyros.mockinterface',
-            'pyros.mockinterface.tests',
-            'pyros.rosinterface',
-            'pyros.rosinterface.tests',
-            'pyros.rosinterface.rostests',
-            'pyros.zmpinterface',
-            'pyros.zmpinterface.tests',
-        ],
-        package_dir={
-            'zmp': 'src/zmp',
-            'pyros': 'src/pyros',
-        },
-        # this is better than using package data ( since behavior is a bit different from distutils... )
-        include_package_data=True,  # use MANIFEST.in during install.
-        install_requires=[
-            'tblib',  # this might not always install six (latest version does not)
-            'six',
-            'pyzmq',
-            'pyros_setup',
-            #'dill',
-        ],
-        zip_safe=False,  # TODO testing...
-    )
+setup(name='pyros',
+    version='0.1.0',
+    description='ROS Node to provide ROS introspection for non-ROS users.',
+    url='http://github.com/asmodehn/pyros',
+    author='AlexV',
+    author_email='asmodehn@gmail.com',
+    license='BSD',
+    packages=[
+        'zmp',
+        'pyros',
+        'pyros.tests',
+        'pyros.baseinterface',
+        'pyros.mockinterface',
+        'pyros.mockinterface.tests',
+        'pyros.rosinterface',
+        'pyros.rosinterface.tests',
+        'pyros.rosinterface.rostests',
+        'pyros.zmpinterface',
+        'pyros.zmpinterface.tests',
+    ],
+    package_dir={
+        'zmp': 'src/zmp',
+        'pyros': 'src/pyros',
+    },
+    # this is better than using package data ( since behavior is a bit different from distutils... )
+    include_package_data=True,  # use MANIFEST.in during install.
+    install_requires=[
+        'tblib',  # this might not always install six (latest version does not)
+        'six',
+        'pyzmq',
+        'pyros_setup',
+    ],
+    zip_safe=False,  # TODO testing...
+)
