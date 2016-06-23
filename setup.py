362364756fb3a9d5b69a42755ccb6c54fcae9aa7
alexv <asmodehn@gmail.com>
2016-06-23 15:31:21 +0900

    getting pyzmp 0.0.11 via dependencies to hopefully fix travis. not using requirements any longer since we dont have extra dependencies and catkin_pip_setup does install the package in dev mode.

--- setup.py
+++ setup.py
@@ -33,12 +33,11 @@ setup(name='pyros',
         'tblib',  # this might not always install six (latest version does not)
         'six',
         'pyzmq',
-        'pyzmp>0.0.9',
+        'pyzmp==0.0.11',  # lets be rigorous since we are working both at the same time...
         'pyros_setup>=0.1.0',  # Careful : pyros-setup < 0.0.8 might already be installed as a deb in /opt/ros/indigo/lib/python2.7/dist-packages/
         'nose>=1.3.7',
     ],
     test_suite="nose.collector",
-    tests_require=[
-    ],
+    tests_require=["nose"],
     zip_safe=False,  # TODO testing...
 )
