32b713bbdc68ade85f193d54f2751437aad694d1
alexv <asmodehn@gmail.com>
2016-08-25 19:17:27 +0900

    releasing 0.0.9 for gopher benevolent

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
 else:  # using setuptools : http://pythonhosted.org/setuptools/
 
     setup(name='pyros',
-        version='0.1.0',
+        version='0.0.9',
         description='ROS Node to provide ROS introspection for non-ROS users.',
         url='http://github.com/asmodehn/pyros',
         author='AlexV',
