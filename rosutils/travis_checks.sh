ae13d79d987250cbb3cd07998d79075a354d6caa
alexv <asmodehn@gmail.com>
2016-08-31 15:28:32 +0900

    moving on with step by step rostesting and partial python testing, because of process conflicts.

--- rosutils/travis_checks.sh
+++ rosutils/travis_checks.sh
@@ -17,5 +17,16 @@ elif [ "$ROS_FLOW" == "install" ]; then
     source install/setup.bash
     echo PYTHONPATH = $PYTHONPATH
     rospack profile
-    python -m nose pyros -s
+
+    python -m nose pyros.mockinterface -s
+    python -m nose pyros.rosinterface.tests -s
+
+    # Careful with process conflicting here
+    python -m nose pyros.rosinterface.rostests.testService -s
+    python -m nose pyros.rosinterface.rostests.testStringTopic -s
+    python -m nose pyros.rosinterface.rostests.testRosInterface -s
+    python -m nose pyros.rosinterface.rostests.testRosInterfaceCache -s
+
+    python -m nose pyros.tests -s
+
 fi
