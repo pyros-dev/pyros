b461567516fbcc037eeb340fd1a0186ebbc15ae5
alexv <asmodehn@gmail.com>
2016-09-02 21:02:03 +0900

    now travis tests with docker and on kinetic.

--- travis_checks.sh
+++ travis_checks.sh
@@ -2,11 +2,15 @@
 set -e
 
 # These variables need to be setup before calling this script:
+# ROS_DISTRO [indigo | jade | kinetic]
 # ROS_FLOW [devel | install]
 
+source /opt/ros/$ROS_DISTRO/setup.bash
+mkdir -p build
 cd build
-
+cmake .. -DCMAKE_INSTALL_PREFIX=./install
 if [ "$ROS_FLOW" == "devel" ]; then
+    make -j1
     source devel/setup.bash
     echo PYTHONPATH = $PYTHONPATH
     rospack profile
@@ -14,6 +18,7 @@ if [ "$ROS_FLOW" == "devel" ]; then
     make -j1 run_tests
     catkin_test_results .
 elif [ "$ROS_FLOW" == "install" ]; then
+    make -j1 install
     source install/setup.bash
     echo PYTHONPATH = $PYTHONPATH
     rospack profile
@@ -28,5 +33,4 @@ elif [ "$ROS_FLOW" == "install" ]; then
     python -m nose pyros.rosinterface.rostests.testRosInterfaceCache -s
 
     python -m nose pyros.tests -s
-
 fi
